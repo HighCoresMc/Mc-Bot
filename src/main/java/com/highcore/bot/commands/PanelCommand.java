b0e107e0da677bdf81c9ea72560e696f6ab006a0
Omarayman008 <Omarayman008@users.noreply.github.com>
2026-05-24 11:11:03 +0300

    Refactors JDA component imports
    Updates import statements to align with the new package structure for JDA components and modals. This change reflects recent refactorings within the JDA library.

--- src/main/java/com/highcore/bot/commands/PanelCommand.java
+++ src/main/java/com/highcore/bot/commands/PanelCommand.java
@@ -8,11 +8,11 @@ import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.components.ActionRow;
-import net.dv8tion.jda.api.interactions.components.buttons.Button;
-import net.dv8tion.jda.api.interactions.components.text.TextInput;
-import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
-import net.dv8tion.jda.api.interactions.modals.Modal;
+import net.dv8tion.jda.api.components.actionrow.ActionRow;
+import net.dv8tion.jda.api.components.buttons.Button;
+import net.dv8tion.jda.api.components.textinput.TextInput;
+import net.dv8tion.jda.api.components.textinput.TextInputStyle;
+import net.dv8tion.jda.api.modals.Modal;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.Color;
